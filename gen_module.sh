c25ebc12f4b67e289a1f59c1e9561fd6f3f8ef01
Mohamed Ali Hachicha <mohamed.ali.hachicha@tekab.dev>
2023-02-06 15:52:35 +0000

    test

--- gen_module.sh
+++ gen_module.sh
@@ -1,3 +1,50 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "hi"
+### Colors ##
+ESC=$(printf '\033') RESET="${ESC}[0m" BLACK="${ESC}[30m" RED="${ESC}[31m"
+GREEN="${ESC}[32m" YELLOW="${ESC}[33m" BLUE="${ESC}[34m" MAGENTA="${ESC}[35m"
+CYAN="${ESC}[36m" WHITE="${ESC}[37m" DEFAULT="${ESC}[39m"
+
+### Color Functions ##
+
+greenprint() { printf "${GREEN}%s${RESET}\n" "$1"; }
+blueprint() { printf "${BLUE}%s${RESET}\n" "$1"; }
+redprint() { printf "${RED}%s${RESET}\n" "$1"; }
+yellowprint() { printf "${YELLOW}%s${RESET}\n" "$1"; }
+magentaprint() { printf "${MAGENTA}%s${RESET}\n" "$1"; }
+cyanprint() { printf "${CYAN}%s${RESET}\n" "$1"; }
+
+fn_bye() { echo "$(magentaprint 'Tekab Module Generator: EXIT')"; exit 0; }
+fn_fail() { echo "Wrong option." exit 1; }
+
+generate_module(){
+case $1 in 
+"Social Media Module")
+    echo "hi";;
+*) 
+echo "unknown";;
+esac
+}
+
+mainmenu() {
+    echo -ne "
+$(magentaprint 'MAIN MENU')
+$(greenprint '1)') CMD1
+$(redprint '0)') Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+    1)
+        generate_module "Social Media Module"
+        mainmenu
+        ;;
+    0)
+        fn_bye
+        ;;
+    *)
+        fn_fail
+        ;;
+    esac
+}
+
+mainmenu
